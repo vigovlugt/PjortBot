b85df5f47d9bc663ac8494b3e7bdfe7527c9568a
vigovlugt <vigo@vlugt.net>
2017-07-02 12:29:17 +0200

    pjortmoneys

--- bot.js
+++ bot.js
@@ -1,19 +1,31 @@
 const Discord = require('discord.js');
-const readline = require('readline');
 const client = new Discord.Client();
+const readline = require('readline');
 const reactions = require('./reactions.js');
 const fs = require("fs");
 var reactionsDict = require('./reactions.json');
+var pjortMoneys = require('./pjortmoneys.json');
+
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+
+var pjortMoneysDict = {};
+
 //SETTINGS
 let prefix = "/";
-let defaultChannel = "212202805467938816";
+let defaultChannel = "329601822132273152";
 
+
+client.on('ready', () => {
+  console.log(' I am ready!');
+  setInterval(oneHour,3600000);
+  setupPjortMoneys();
+
+});
 
 //defaultChannel = client.channels.get(defaultChannel);
 console.log(defaultChannel);
@@ -66,10 +78,7 @@ rl.on('line', (input) => {
 
 });
 
-client.on('ready', () => {
-  console.log(' I am ready!');
-  setInterval(tenMin,600000);
-});
+
 
 
 
@@ -85,35 +94,145 @@ client.on('message', message => {
     for (var i = 1; i < command.length; i++) {
       args += " " + command[i];
     }
+    let splittedArgs = args.split(",");
+    console.log(splittedArgs);
+
+
+    for (var i = 0; i < splittedArgs.length; i++) {
+      while (splittedArgs[i].charAt(0) == " ") {
+        splittedArgs[i] = splittedArgs[i].substring(1);
+      }
+    }
+
 
     switch (command[0]) {
       case "pjort":
+      case "help":
+      case "p":
         help(message);
         break;
-
+        // REACTIONS
       case "addreaction":
-        let splittedArgs = args.split(",");
-        console.log(splittedArgs);
-
+      case "ar":
         if(splittedArgs.length != 2)
         {
           return;
         }
-        for (var i = 0; i < splittedArgs.length; i++) {
-          while (splittedArgs[i].charAt(0) == " ") {
-            splittedArgs[i] = splittedArgs[i].substring(1);
-          }
+        let newReact = {"message": splittedArgs[0], "reaction": splittedArgs[1] };
+        console.log("Message added:");
+        console.log(JSON.stringify(newReact));
+        reactionsDict.push(newReact);
+        console.log(reactionsDict);
+        fs.writeFile("./reactions.json",JSON.stringify(reactionsDict),(err) => {console.log(err);});
+        break;
+
+
+
+      //PJORTMONEYS
+      case "pjortmoney":
+      case "$":
+
+        if(pjortMoneysDict[message.author.id] != null){
+          message.channel.send(pjortMoneys[pjortMoneysDict[message.author.id]].money + "$");
+        }else{
+          registerPjortMoneys(message.author.id);
+          message.channel.send("0$");
         }
 
-        let newReact = "{\"message\":\"" + splittedArgs[0] + "\",\"reaction\":\"" + splittedArgs[1] + "\"}";
-        console.log("Message added:");
-        console.log(newReact);
-        reactionsDict.push(JSON.parse(newReact));
-        console.log(reactionsDict);
-        fs.writeFile("./reactions.json",JSON.stringify(reactionsDict));
         break;
-      }
 
+        // STEENPAPIERSCHAAR
+        case "sps":
+
+        if(isNaN(command[2])){
+          message.reply("je moet me niet testen");
+          return;
+        }
+
+
+
+          if(pjortMoneys[pjortMoneysDict[message.author.id]].money < parseInt(command[2])){
+            message.reply("je bent te brokko");
+            break;
+          }
+
+          let pjortSps = Math.floor(Math.random() * 3);
+          let playerSps = command[1];
+          let winner;
+
+          if(command[1] == "steen"){
+          playerSps = 0;
+        }else if (command[1] == "papier") {
+          playerSps = 1;
+        }else if (command[1] == "schaar") {
+          playerSps = 2;
+        }else if(command[1] == (0 || 1 || 2)){
+          message.reply("je moet me niet testen");
+          break;
+        }
+
+
+          if(pjortSps == playerSps){
+            message.reply("gelijkspel, goed gestreden");
+            break;
+          }else if (pjortSps == 0 && playerSps == 1){
+            winner = "player";
+          }else if (pjortSps == 0 && playerSps == 2) {
+            winner = "pjort";
+          }else if (pjortSps == 1 && playerSps == 0) {
+            winner = "pjort"
+          }else if (pjortSps == 1 && playerSps == 2) {
+            winner = "player"
+          }else if (pjortSps == 2 && playerSps == 0) {
+            winner = "player"
+          }else if (pjortSps == 2 && playerSps == 1) {
+            winner = "pjort"
+          }
+
+          let pjortChoice;
+
+          switch (pjortSps) {
+            case 0:
+              pjortChoice = "steen";
+              break;
+            case 1:
+              pjortChoice = "papier";
+              break;
+            case 2:
+              pjortChoice = "schaar";
+            break;
+          }
+
+
+
+          message.channel.send("ik kies jou! " + pjortChoice);
+
+          if(winner == "pjort"){
+            message.reply("ik win, natuurlijk, natuurlijk");
+
+
+          }else{
+            message.reply("ik krijg jou nog wel te pakken ventje");
+          }
+
+          let bet = parseInt(command[2]);
+
+          if(bet == null){
+            break;
+          }
+
+          if(winner == "pjort"){
+            pjortMoneys[pjortMoneysDict[message.author.id]].money -= bet ;
+          }else{
+            pjortMoneys[pjortMoneysDict[message.author.id]].money += bet;
+          }
+          savePjortMoneys();
+          message.channel.send("nu heb je: " + pjortMoneys[pjortMoneysDict[message.author.id]].money + "$")
+
+
+          break;
+
+        }
 
   }else{
 
@@ -137,26 +256,54 @@ client.on('message', message => {
       }
     }
 
+    switch (message.content) {
+      case "pjort je bent lief":
+        pjortMoneys[pjortMoneysDict[message.author.id]].money += 1;
+        message.channel.send("nu heb je " + pjortMoneys[pjortMoneysDict[message.author.id]].money + "$");
+        break;
+      case "pjort, ik zou je doen":
+        pjortMoneys[pjortMoneysDict[message.author.id]].money += 2;
+        message.channel.send("nu heb je " + pjortMoneys[pjortMoneysDict[message.author.id]].money + "$");
+        break;
+
+    }
+
+
   }
 
 });
 
-function tenMin(){
+function setupPjortMoneys(){
+  for (var i = 0; i < pjortMoneys.length; i++) {
+      pjortMoneysDict[pjortMoneys[i].id] = i;
+  }
+}
+
+function savePjortMoneys(){
+  fs.writeFile("./pjortmoneys.json",JSON.stringify(pjortMoneys),(err) => {console.log(err);});
+}
+
+function oneHour(){
   setAvatar();
   sendRandomMessage();
 }
 
+function registerPjortMoneys(id){
+  let newUser = {"id":id,"money":0};
+  pjortMoneys.push(newUser);
+  savePjortMoneys();
+}
 
 function help(message){
   var help = "goedendag, ik ben pjort. wat wilt u tegen mij zeggen?";
   for (var i = 0; i < reactionsDict.length; i++) {
-    help += "\n -" + reactionsDict[i].message;
+    help += "\n - " + reactionsDict[i].message;
   }
 
   help += "\n";
 
   for (var i = 0; i < avatarMessages.length; i++) {
-    help += "\n -" + avatarMessages[i].message;
+    help += "\n - " + avatarMessages[i].message;
   }
   message.channel.send(help);
 }
@@ -200,6 +347,14 @@ var randomMessages = [
   {"message":"ik wil kaas"},
   {"message":"ik ben te cool voor jullie allemaal"},
   {"message":"aambeien > aardbeien"},
+  {"message":"ik ga zo dik"},
+  {"message":"ik ben zo fk hard"},
+  {"message":"je moet me niet schreeuwen"},
+  {"message":"jezus wat ben ik cool"},
+  {"message":"slapen is voor de zwakken"},
+  {"message":"#Trump NR.1"},
+  {"message":"altijd wanneer ik kleine jongentjes zie word mijn penis zo dik"},
+  {"message":"#Trump NR.1"},
 ]
 
 
